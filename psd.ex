1ca96940652d58cc627cd519c47273a4d3eae8d9
Sajid Ali <sajidhgn@gmail.com>
2023-09-26 13:08:10 +0500

    Update psd.ex

--- psd.ex
+++ psd.ex
@@ -1,42 +1,36 @@
-defmodule PasswordGenerator do
+defmodule PasswordGeneratorFun do
   @number_chars "0123456789"
-  @symbol_chars "!@#$%^&*()_+-=[]{}|;:'\",.<>/?"
   @uppercase_chars "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  @symbol_chars "!@#$%^&*()_+-=[]{}|;:'\",.<>/?\\"
 
-  def generate_password(number, symbol, uppercase, length) when is_boolean(number) and is_boolean(symbol) and is_boolean(uppercase) and is_integer(length) and length > 0 do
 
-    alphabet = build_alphabet(number, symbol, uppercase)
+  def genbox (%{"length_field" => length_field, "number_field" => number_field, 
+  "uppercase_field" => uppercase_field, "symbol_field" => symbol_field}) do
 
-    if length <= String.length(alphabet) do
-      password = generate_password_recursive(alphabet, length, "")
-      {:ok, password}
-    else
-      {:error, "Invalid parameter types. Please ensure that number, uppercase, and symbol are booleans, and length is an integer."}
-    end
-  end
+    length = length_field |> String.to_integer()
+    number = if number_field=="true", do: "true", else: ""
+    uppercase = if uppercase_field=="true", do: "true", else: ""
+    symbol = if symbol_field=="true", do: "true", else: ""
 
-  defp build_alphabet(number, symbol, uppercase) do
-    alphabet =
-      (if number, do: @number_chars, else: "") <>
-      (if symbol, do: @symbol_chars, else: "") <>
-      (if uppercase, do: @uppercase_chars, else: "")
+     charset =
+      [
+        (if String.length(symbol) > 0, do: @symbol_chars, else: ""),
+        (if String.length(uppercase) > 0, do: @uppercase_chars, else: ""),
+        (if String.length(number) > 0, do: @number_chars, else: "")
+      ]
+      |> Enum.filter(&(&1 != ""))
+      |> Enum.join()
 
-    alphabet
-  end
+       char_list = String.graphemes(charset)
 
-  defp generate_password_recursive(_, 0, password) do
-    password
-  end
-  defp generate_password_recursive(alphabet, length, password) do
-    random_index = :rand.uniform(String.length(alphabet))
-    random_char = String.at(alphabet, random_index)
-    IO.puts(random_char)
-    generate_password_recursive(alphabet, length - 1, password <> random_char)
+        password = Enum.reduce(1..length, "", fn _i, acc ->
+        char = Enum.random(char_list)
+        acc <> char
+      end)
+
+      IO.puts(password)
   end
 end
 
-{:ok, password} = PasswordGenerator.generate_password(true, true, false, 20)
-IO.puts(password)
-
-{:error, error_message} = PasswordGenerator.generate_password(false, false, false, 8)
-IO.puts(error_message)
+PasswordGeneratorFun.genbox(%{"length_field" => "12", "number_field" => "false", 
+  "uppercase_field" => "true", "symbol_field" => "true"})
